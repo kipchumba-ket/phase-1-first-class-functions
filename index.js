c711e4f86375ce459e7012c1025660efb709115d
kipchumba-ket <kipchumbaelly97@gmail.com>
2022-11-23 14:31:36 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+    callback();
+}
+function returnsANamedFunction(){
+    return function namedFuction(){
+        return "be cool"
+    }
+}
+function returnsAnAnonymousFunction(){
+    return  () => 'before all';
+ }
+  
